f047dd2473fac89313db7a0c45cac1c799594883
Izhcel Arzate Miranda <itzelarzamiran13825@gmail.com>
2026-02-21 12:29:29 -0600

    prueba

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,23 +4,9 @@ import Presentation from "../components/Presentation/Presentation";
 
 function Home() {
   return (
-    <Layout>
-      <Cover
-        title="Universidad Politécnica de Atlacomulco"
-        studentName="Izhcel Arzate Miranda"
-        subject="Programación Web"
-        group="ISC"
-      />
-      <Presentation
-        curso="Programación Web"
-        area="Ingeniería en Sistemas Computacionales"
-        alumno="Izhcel Arzate Miranda"
-        matricula="2331107051"
-        grupo="ISC"
-        periodoEscolar="Enero - Mayo 2026"
-        institucion="Universidad Politécnica de Atlacomulco"
-      />
-    </Layout>
+    <div>
+        <h1>Test</h1>
+    </div>
   );
 }
 
